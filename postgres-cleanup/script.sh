f1f2eeaf576bdf50c497963e8caa9d41670f3311
Evan Shortiss <evanshortiss@gmail.com>
2023-08-21 11:28:01 -0700

    feat: support variable for deactivate query time

--- postgres-cleanup/script.sh
+++ postgres-cleanup/script.sh
@@ -1,5 +1,8 @@
-echo "Deactivating old links...\n"
+echo "Deactivating old links"
 
-psql -c "UPDATE link SET active = false WHERE create_ts < CURRENT_DATE - INTERVAL '1 days';"
+QUERY="UPDATE link SET active = false WHERE create_ts < CURRENT_TIMESTAMP - INTERVAL '$LINK_TTL_HOURS hours';"
+echo "Query: \"$QUERY\""
+
+psql -c "$QUERY"
 
 echo "\nFinished deactivating old links"
