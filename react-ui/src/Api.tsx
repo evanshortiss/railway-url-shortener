834b3f5f92196bf08f435f58be2496c5605692db
Evan Shortiss <evanshortiss@gmail.com>
2023-08-21 10:22:02 -0700

    fix: handle non 200 status codes from API

--- react-ui/src/Api.tsx
+++ react-ui/src/Api.tsx
@@ -13,5 +13,9 @@ export async function createtLink (url: string): Promise<Link> {
     body: url
   })
 
+  if (res.status !== 200) {
+    throw new Error(`received ${res.status} response when trying to create shortlink`)
+  }
+
   return res.json() as Promise<Link>
 }
