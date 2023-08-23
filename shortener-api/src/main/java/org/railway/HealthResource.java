0b48a3556cbf0532af948a2d2a8ee1d0afba402e
Evan Shortiss <evanshortiss@gmail.com>
2023-08-23 16:07:34 -0700

    fix: add database conncetivity check to health endpoint

--- shortener-api/src/main/java/org/railway/HealthResource.java
+++ shortener-api/src/main/java/org/railway/HealthResource.java
@@ -1,15 +1,40 @@
 package org.railway;
 
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.jboss.logging.Logger;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/health")
 public class HealthResource {
+    @Inject
+    Mutiny.SessionFactory sessionFactory;
+
+    private static final Logger Log = Logger.getLogger(HealthResource.class);
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Health getHealth() {
-        return new Health();
+    public Uni<Response> getHealth() {
+        Log.debug("Running health check");
+        return sessionFactory.withTransaction((session, transaction) -> {
+            return session.createNativeQuery("SELECT 1")
+                    .getSingleResult()
+                    .map((result) -> {
+                        if (result.equals(1)) {
+                            Log.debug("Health check passed. Returning HTTP 200");
+                            return Response.ok(new Health()).build();
+                        } else {
+                            Log.warn("Health check failed due to broken database connectivity. Returning HTTP 500.");
+                            return Response.serverError().entity("").build();
+                        }
+                    })
+                    .onFailure().recoverWithItem(Response.serverError().build());
+        });
     }
 }
