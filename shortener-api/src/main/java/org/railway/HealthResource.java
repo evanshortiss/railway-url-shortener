0d4f699d27a9cacf95917518d70880ab48f8fc47
Evan Shortiss <evanshortiss@gmail.com>
2023-08-24 23:13:50 -0700

    feat: add self healthcheck

--- shortener-api/src/main/java/org/railway/HealthResource.java
+++ shortener-api/src/main/java/org/railway/HealthResource.java
@@ -3,6 +3,7 @@ package org.railway;
 import org.hibernate.reactive.mutiny.Mutiny;
 import org.jboss.logging.Logger;
 
+import io.quarkus.scheduler.Scheduled;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -21,14 +22,49 @@ public class HealthResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> getHealth() {
-        Log.info("Running health check");
+        Log.debug("Running health check due to HTTP request");
+        
+        return hasDatabaseConnectivity().map((connected) -> {
+            if (connected == true) {
+                Log.debug("Health check passed. Returning HTTP 200.");
+                return Response.ok(new Health()).build();
+            } else {
+                Log.error("Health check failed: DB unavailable. Returning HTTP 500.");
+                return Response.serverError().build();
+            }
+        });
+    }
+    
+    @Scheduled(every = "8s", delayed = "5s")
+    Uni<Void> checkDatabaseConnectivity () {
+        Log.debug("Running health check due to schedule.");
+        return hasDatabaseConnectivity().map((connected) -> {
+            if (connected == true) {
+                Log.debug("Health check passed.");
+                return null;
+            } else {
+                Log.error("Health check failed: DB unavailable.");
+                return null;
+            }
+        });
+    }
+
+    Uni<Boolean> hasDatabaseConnectivity () {
         return sessionFactory.withTransaction((session, transaction) -> {
             return session.createNativeQuery("SELECT 1")
                 .getSingleResult()
                 .map((result) -> {
-                    Log.info("Health check passed. Returning HTTP 200");
-                    return Response.ok(new Health()).build();
+                    Log.debug("Health check database query complete.");
+                    return true;
+                })
+                .onFailure().recoverWithUni(() -> {
+                    Log.error("Health check failed on Postgres query.");
+                    return Uni.createFrom().item(false);
                 });
-        });
+            })
+            .onFailure().recoverWithUni(() -> {
+                Log.error("Health check failed before executing Postgres query.");
+                return Uni.createFrom().item(false);
+            });
     }
 }
